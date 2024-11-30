56b32fff0cc34f5c297596644e7f926119712804
alauracarlotta <carlotta.custodio@gmail.com>
2024-11-30 14:14:19 -0300

    feat(recipes): modify templates BASE_DIR setings with example

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.recipes.views import home, about
+from apps.recipes.views import contact, home, about
 
 
 urlpatterns = [
     path('', home),
+    path('contact/', contact),
     path('about/', about),
 ]
--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -4,8 +4,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    name = 'Laura'
+    return render(request, 'recipes/home.html', 
+        context = {
+            'name': name    
+        }, 
+        status = 200
+    )
 
+def contact(request):
+    teste = 'A vida de Laura é Bela!'
+    return render(request, 'correct/temp.html',
+        context = {
+            'foi': teste
+        }
+    )
 
 def about(request):
     return HttpResponse("Hello, Django!")
