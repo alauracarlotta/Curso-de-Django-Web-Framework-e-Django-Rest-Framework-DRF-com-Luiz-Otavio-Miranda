14f153e77d86c7f481d6915bf31038f565fea415
alauracarlotta <carlotta.custodio@gmail.com>
2024-11-30 21:48:20 -0300

    style(home.html): add style guide from recipe card description

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -5,9 +5,23 @@ from django.shortcuts import render
 
 def home(request):
     name = 'Laura'
+    author_date = '30/11/2024 às 8:44pm'
+    category = {
+        'doces': 'Doces',
+        'salgado': 'salgado',
+        'bebida': 'bebida'
+    }
+    ingredients = [
+        '2 ovos',
+        '3 xicaras de leite',
+        '4 azeitonas'
+    ]
     return render(request, 'pages/home.html', 
         context = {
-            'name': name    
+            'name': name,
+            'author_date': author_date,
+            'category': category['doces'],
+            'ingredients': ingredients
         }, 
         status = 200
     )
