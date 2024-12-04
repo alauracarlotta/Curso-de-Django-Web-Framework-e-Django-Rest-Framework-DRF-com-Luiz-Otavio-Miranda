c9d0e41e35c5e93103b29dfa9f8602a566fed252
alauracarlotta <carlotta.custodio@gmail.com>
2024-12-04 18:08:33 -0300

    test(tests.py): add test class RecipeURLsTest

--- apps/recipes/tests.py
+++ apps/recipes/tests.py
@@ -3,4 +3,6 @@ from django.test import TestCase
 
 class RecipeURLsTest(TestCase):
     def test_pytest_is_ok(self):
-        ...
+        # var_test = 30 (inserir breakpoint abaixo dessa var e usar o debug)
+        print('OLÁ, MUNDO!')
+        assert 1 == 1
